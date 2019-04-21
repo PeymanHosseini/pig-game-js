db096e0e15d1341f296ba9d07d89d89aee9219e8
Peyman Hosseini <peyman.66h@gmail.com>
2019-04-21 16:41:26 +0300

    dice function

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var player, roundScore, totalScore, activePlayer;
+player = [0, 0];
+roundScore = 0;
+totalScore = 0;
+activePlayer = 0;
+
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var dice = Math.floor(Math.random() * 6) + 1;
+});
